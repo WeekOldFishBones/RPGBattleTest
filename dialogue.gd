cb54b5bd5abe2f6211203661af407cc9e7e55b91
FishBones <117949230+WeekOldFishBones@users.noreply.github.com>
2025-06-10 10:43:24 +0100

    update v0.04
    updated flow chart with optimisations. made dialogue update with just one line, using two functions. added continue_icon.

--- dialogue.gd
+++ dialogue.gd
@@ -10,7 +10,6 @@ var enemy_dict = {"name": "Great King Crow"}
 signal message_complete()
 
 var replacement_regex = RegEx.new()
-var message_array = []
 var replacement_dict = {
 	"PLAYER": player_dict.name,
 	"ENEMY": enemy_dict.name
@@ -18,15 +17,9 @@ var replacement_dict = {
 var output_message
 
 func _ready() -> void:
-	update_message("[color=black][wave amplitude=5]This is a text box with dialogue, $PLAYER$'s name, and $ENEMY$'s name in it![/wave][/color]")
-	placeholder_splitter(content.text)
-	update_message(replace_placeholders(message_array, replacement_dict))
+	update_message(replace_placeholders("[color=black][wave amplitude=5]This is a text box with dialogue, $PLAYER$'s name, and $ENEMY$'s name in it![/wave][/color]", replacement_dict))
 
-func update_message(message) -> void:
-	content.text = message
-	content.visible_characters = 0
-	type_timer.start()
-
+#display text content in typewriter style
 func _on_type_timer_timeout() -> void:
 	if content.visible_characters < content.text.length():
 		content.visible_characters += 1
@@ -34,18 +27,25 @@ func _on_type_timer_timeout() -> void:
 		type_timer.stop()
 		message_complete.emit()
 
-func placeholder_splitter(message):
+#update content.text
+func update_message(message) -> void:
+	content.text = message
+	content.visible_characters = 0
+	type_timer.start()
+
+#replace placeholders in input text
+func replace_placeholders(message : String, replacements : Dictionary):
+	var message_array = []
+	#split message by placeholders and assign to message_array
 	message_array = message.split("$", true, 0)
 	print(message_array)
-
-func replace_placeholders(input_array : Array, replacements : Dictionary):
-	var new_message_array = []
-	message_array = input_array
+	#make a new_message_array to store the final string
+	var output_message_array = []
 	replacement_dict = replacements
-	for i in input_array:
+	for i in message_array:
 		if replacements.has(i):
-			new_message_array.push_back(replacements.get(i))
+			output_message_array.push_back(replacements.get(i))
 		else:
-			new_message_array.push_back(i)
-	print(new_message_array)
-	return "".join(new_message_array)
+			output_message_array.push_back(i)
+	print(output_message_array)
+	return "".join(output_message_array)
